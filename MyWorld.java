8afc24a9102e6bfcadec62dd4c549aaf31bde833
Vlasciar <48627697+Vlasciar@users.noreply.github.com>
2019-09-15 16:02:12 +0300

    Movement_done

--- MyWorld.java
+++ MyWorld.java
@@ -28,5 +28,21 @@ public class MyWorld extends World
     {
         Player player = new Player();
         addObject(player,586,327);
+        Wall_Oriz wall_Oriz = new Wall_Oriz();
+        addObject(wall_Oriz,321,112);
+        Wall_Oriz wall_Oriz2 = new Wall_Oriz();
+        addObject(wall_Oriz2,776,104);
+        Wall_Oriz wall_Oriz3 = new Wall_Oriz();
+        addObject(wall_Oriz3,759,529);
+        Wall_Oriz wall_Oriz4 = new Wall_Oriz();
+        addObject(wall_Oriz4,302,513);
+        Wall_Vert wall_Vert = new Wall_Vert();
+        addObject(wall_Vert,256,459);
+        Wall_Vert wall_Vert2 = new Wall_Vert();
+        addObject(wall_Vert2,270,149);
+        Wall_Vert wall_Vert3 = new Wall_Vert();
+        addObject(wall_Vert3,820,148);
+        Wall_Vert wall_Vert4 = new Wall_Vert();
+        addObject(wall_Vert4,806,483);
     }
 }
--- Player.java
+++ Player.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Player here.
@@ -6,147 +6,101 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Player extends Actor
-{
+public class Player extends Actor {
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
-    {
-        Check_Movement();
-    }  
-    int speed=3;//viteza de miscare
-    int pX;//player x
-    int pY;//player y
-    
-    
-    private void Check_Movement()
-    {
-       pX=getX();
-       pY=getY(); 
-       
-       
-       if((Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("D")) && 
-       (Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("W"))){
-         turnTowards(pX+1,pY-1);         
-         if(check_move()==true)
-         {
-         this.setLocation(pX+speed,pY-speed);
-        }
-        if(check_move()==false)
-        {
-         this.setLocation(pX-speed,pY+speed);
-        } 
-        
-        }
-        
-        /////////////////////////
-         else if((Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("D")) 
-         && (Greenfoot.isKeyDown("down")|| Greenfoot.isKeyDown("S")) ){
-        turnTowards(pX+1,pY+1);
-        if(check_move()==true)
-         {
-         this.setLocation(pX+speed,pY+speed);
-        }
-        if(check_move()==false)
-        {
-         this.setLocation(pX-speed,pY-speed);
-        }
-        }
-        
-        ///////////////////////////////
-         else if((Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("A")) &&
-          (Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("W"))){
-         turnTowards(pX-1,pY-1);
-         if(check_move()==true)
-         {
-         this.setLocation(pX-speed,pY-speed);
-        }
-        if(check_move()==false)
-        {
-         this.setLocation(pX+speed,pY+speed);
-        }
-        }
-        
-        /////////////////
-        else if((Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("A"))&&
-          (Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("S"))){
-         turnTowards(pX-1,pY+1);
-         if(check_move()==true)
-         {
-         this.setLocation(pX-speed,pY+speed);
-        }
-        if(check_move()==false)
-        {
-         this.setLocation(pX+speed,pY-speed);
-        }
-        }
-         
-       ////////////////
-        else if((Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("D"))){
-         turnTowards(pX+1,pY);         
-        if(check_move()==true)
-         {
-         this.setLocation(pX+speed,pY);
-        }
-        if(check_move()==false)
-        {
-         this.setLocation(pX-speed,pY);
-        }
-        }
-        
-        ////////////////////////
-        else if((Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("A"))){
-         turnTowards(pX-1,pY);         
-         if(check_move()==true)
-         {
-         this.setLocation(pX-speed,pY);
-        }
-        if(check_move()==false)
-        {
-         this.setLocation(pX+speed,pY);
-        }        
-        }
-        
-        /////////////////////////
-        else if((Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("W")))
-        {
-         turnTowards(pX,pY-1);
-        if(check_move()==true)
-        {
-         this.setLocation(pX,pY-speed);
-        }
-        if(check_move()==false)
-        {
-         this.setLocation(pX,pY+speed);
-        }
-        }
-        
-        ////////////////////////
-        else if((Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("S"))){ 
-         turnTowards(pX,pY+1);         
-         if(check_move()==true)
-         {
-         this.setLocation(pX,pY+speed);
-        }
-        if(check_move()==false)
-        {
-         this.setLocation(pX,pY-speed);
-        }
-        } 
+    public void act() {
+        Movement();
     }
-    public boolean check_move()//coliziuni cu obstacolele
-     {
-         Actor props = getOneIntersectingObject(Props.class);
-
-         if(props == null)       
-            return true;
-            else {
-                return false;
+    int speed_diagonal=2; //viteza pe diagonala
+    int speed_normal=3;
+    int pX; //player x
+    int pY; //player y
+    private void DoMove(int next_pX, int next_pY)//miscare diagonala
+    {
+        pX = getX();
+        pY = getY();
+        this.setLocation(next_pX, next_pY);
+        if (check_move() == false) {
+            this.setLocation(pX, pY);
+        }
+        if (getX() == pX && getY() == pY) {
+            this.setLocation(next_pX, pY);
+            if (check_move() == false) {
+                this.setLocation(pX, pY);
             }
-        
-            
-        
-     }
+        }
+        if (getX() == pX && getY() == pY) {
+            this.setLocation(pX, next_pY);
+            if (check_move() == false) {
+                this.setLocation(pX, pY);
+            }
+        }
+    }
+
+    private void Movement() {
+        pX = getX();
+        pY = getY();
+        if ((Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("D")) &&
+        (Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("W"))) {
+            // turnTowards(pX+1,pY-1);                     
+            DoMove(pX+speed_diagonal,pY-speed_diagonal);
+        }////////////////////
+        else if ((Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("D")) &&
+        (Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("S"))) {
+            // turnTowards(pX+1,pY+1);            
+            DoMove(pX+speed_diagonal,pY+speed_diagonal);
+        }///////////////////////////////
+        else if ((Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("A")) &&
+        (Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("W"))) {
+            // turnTowards(pX-1,pY-1);           
+            DoMove(pX-speed_diagonal,pY-speed_diagonal);
+        }        /////////////////
+        else if ((Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("A")) &&
+        (Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("S"))) {
+            //  turnTowards(pX-1,pY+1);            
+            DoMove(pX-speed_diagonal,pY+speed_diagonal);
+        }        ////////////////
+        else if ((Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("D"))) {
+            //  turnTowards(pX+1,pY);         
+            this.setLocation(pX + speed_normal, pY);
+            if (check_move() == false) {
+                this.setLocation(pX, pY);
+            }
+        }        ////////////////////////
+        else if ((Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("A"))) {
+            //   turnTowards(pX-1,pY);         
+            this.setLocation(pX - speed_normal, pY);
+            if (check_move() == false) {
+                this.setLocation(pX, pY);
+            }
+        }        /////////////////////////
+        else if ((Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("W"))) {
+            //   turnTowards(pX,pY-1);
+            this.setLocation(pX, pY - speed_normal);
+            if (check_move() == false) {
+                this.setLocation(pX, pY);
+            }
+        }        ////////////////////////
+        else if ((Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("S"))) {
+            //  turnTowards(pX,pY+1);         
+            this.setLocation(pX, pY + speed_normal);
+            if (check_move() == false) {
+                this.setLocation(pX, pY);
+            }
+        }
+    }
+
+    public boolean check_move() //coliziuni cu obstacolele
+    {
+        Actor prop = getOneIntersectingObject(Props.class);
+        if (prop == null)
+            return true;
+        else {
+            return false;
+        }
+
+    }
 }
